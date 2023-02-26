22fc165ec03b0c1b852fcde2f8efc4f7137c298f
thepwnexperts <thepwnexperts@gmail.com>
2023-02-26 12:17:36 +0000

    [fix] information disclosure

--- app.js
+++ app.js
@@ -1,48 +1,32 @@
 const express = require('express');
 var compression = require('compression');
 const app = express();
-const mongoose = require('mongoose');
+const db = require('mongoose');
 const dotenv=require('dotenv');
 //dotenv.config();
 dotenv.config({ path: './.env' })
 const cors=require('cors');
 
-//use
+//mid 
 app.use(express.json());
 app.use(cors());
 app.use(compression());
 
-//names
-//routes
-
-//auth
 
 const cal =require('./routes/calculate');
 
-//routes 
-//remote address
-//app.use("/api/users",userRoter);
-//app.use("/user",authRoute);
+
+
+//path
 app.use("/",cal);
 
 
 
-//test
+//db
 
-try{
-    let db_str=process.env.DB_CONNECTION_LOCAL;
-    mongoose.connect("mongodb+srv://test:test@test.e3l1a.mongodb.net/tpe?retryWrites=true&w=majority")
-  .then(() => console.log('DB Connection Successful'));
-    //mongoose.connect (db_str, {useNewUrlParser: true});
-    //mongoose.connect('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.1');
-    
-}
-catch(e)
-{
-    console.log(e)
-}
-
-
+db.connect(process.env.dburi)
+  .then(() => console.log('DB Connection Successful'))
+  .catch(err => console.log(`DB Connection Error: ${err.message}`));
 
 
 //local
