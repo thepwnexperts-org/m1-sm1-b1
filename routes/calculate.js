a378c52fb29c786c9f65dcdaea40a1833402cc8f
thepwnexperts <thepwnexperts@gmail.com>
2023-08-03 15:57:36 +0530

    load sample to db

--- routes/calculate.js
+++ routes/calculate.js
@@ -5,6 +5,52 @@ const dotenv = require('dotenv');
 dotenv.config();
 const waf = JSON.parse(process.env.waf);
 
+
+
+const LoadData =   [
+    {
+      "id": "2",
+      "name": "Masala Noodels",
+      "image": "https://c.ndtvimg.com/2022-09/1jsu8038_noodles_625x300_28_September_22.jpg?im=FaceCrop,algorithm=dnn,width=1200,height=886",
+      "price": 150,
+    },
+    {
+      "id": "4",
+      "name": "Mexican Tacos",
+      "image": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-202104-birriatacos-058-1619806492.jpg",
+      "price": 369,
+    },
+    {
+      "id": "5",
+      "name": "Pani Puri",
+      "image": "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201708/dish-story_647_081417052301.jpg?Versionid=VSzUwnMrbhD64jwKU7p.e6KBcJaW9CvU&size=690:388",
+      "price": 80,
+    },
+    {
+      "id": "8",
+      "name": "Sabudhdanna",
+      "image": "https://c.ndtvimg.com/2022-08/ft4hhqjo_sabudana-khichdi_625x300_16_August_22.jpg?im=FeatureCrop,algorithm=dnn,width=620,height=350",
+      "price": 209,
+    },
+    {
+      "id": "9",
+      "name": "Burger",
+      "image": "https://www.ingredion.com/content/dam/ingredion/usca-images/food/meat/cheeseburger-bread_720x560.jpg",
+      "price": 239,
+    },
+    {
+      "id": "6",
+      "name": "Banana",
+      "image": "https://images.unsplash.com/photo-1571771894821-ce9b6c11b08e",
+      "price": 100,
+    }
+  ]
+
+
+
+
+
+
 router.get('/p/:p', (req, res) => { 
 	var limit =5;
 	const page = req.params.p;
@@ -109,6 +155,18 @@ router.post('/bulkCreate', async (req, res) => {
         res.status(400).send(err);
     }
 });
+
+router.post('/load', async (req, res) => {
+    try {
+
+        const items = LoadData;
+        await Item.insertMany(items);
+        res.send("Records inserted successfully.");
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
+
 
 
 
